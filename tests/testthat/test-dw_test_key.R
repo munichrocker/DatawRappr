20d5f0a0a9d5cba346854318f880c9ac8061b523
Benedict Witzenberger <benedict.witzenberger@sueddeutsche.de>
2019-11-26 15:34:25 +0100

    delete key-test

--- tests/testthat/test-dw_test_key.R
+++
@@ -1,9 +0,0 @@
-library(httptest)
-
-with_mock_api({
-  test_that("We can get a user object", {
-    user <- dw_test_key()
-    expect_s3_class(user, "dw_user")
-    expect_equal(user[["response"]][["status_code"]], 200)
-  })
-})
