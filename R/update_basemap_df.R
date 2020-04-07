12c63b95b95c318926dbd8a5ab9f46f31fd6f33c
Benedict Witzenberger <benedict.witzenberger@sueddeutsche.de>
2020-04-07 15:05:12 +0200

    add function to create basemap overview file

---
+++ R/update_basemap_df.R
@@ -0,0 +1,16 @@
+# This script contains a function to update the basemap-df, if needed
+needs::needs(usethis, httr, dplyr, tidyr)
+
+r <- GET("https://api.datawrapper.de/plugin/basemaps")
+list_dw_maps <- content(r)$data
+
+dw_basemaps <- tibble(maps = list_dw_maps)
+
+dw_basemaps %<>%
+  unnest_wider(maps) %>%
+  rename(level = label) %>%
+  unnest(keys) %>%
+  unnest_wider(keys) %>%
+  select(-keys)
+
+usethis::use_data(dw_basemaps)
