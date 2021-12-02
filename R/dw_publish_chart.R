821c6f57993cd4196db0a46f5ecb14a9ebea48c2
Markus Hametner <markus.hametner@sz.de>
2021-12-02 17:39:04 +0100

    [FIX] dw_publish_chart used non-v3 api, leading to 404s. use v3 now

--- R/dw_publish_chart.R
+++ R/dw_publish_chart.R
@@ -38,7 +38,7 @@ dw_publish_chart <- function(chart_id, api_key = "environment", return_urls = TR
 
   chart_id <- dw_check_chart_id(chart_id)
 
-  url <- paste0("https://api.datawrapper.de/charts/", chart_id, "/publish")
+  url <- paste0("https://api.datawrapper.de/v3/charts/", chart_id, "/publish")
 
   r <- dw_call_api("POST", url, httr::add_headers(Authorization = paste("Bearer", api_key, sep = " ")),
                   .DATAWRAPPR_UA, return_raw_response=T)
